53728ac2bd3f45ec63e2b557b72714a56b5fe520
Hovsep Shirinyan <hovsep.shirinyan@yahoo.com>
2021-03-11 23:36:02 +0400

    --

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{display:"flex", justifyContent:"center", alignItems:"center", color:"red",height:"100vh", width:"100vw"}}>
-      <span>comming soon</span>
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center", color:"red",height:"100vh", width:"100vw", }}>
+      <h1>\\\\COMMING SOON////</h1>
     </div>
   );
 }
